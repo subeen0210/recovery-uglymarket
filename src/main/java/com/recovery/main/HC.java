116c15a09dd659a659a704fa02c5545a7a599b9d
Ms <gkekffn@gmail.com>
2023-12-07 15:56:14 +0900

    mz

---
+++ src/main/java/com/recovery/main/HC.java
@@ -0,0 +1,19 @@
+package com.recovery.main;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HC")
+public class HC extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(11);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
