222cd458c0c291be2f101ff9e7a12b6e07688cfb
subeen0210 <subeen0210@gmail.com>
2024-01-03 11:48:51 +0900

    itemDetail.js 안쓰는 코드 주석처리, 별 채우는 코드 작성 중

--- src/main/webapp/wj/js/itemDetail.js
+++ src/main/webapp/wj/js/itemDetail.js
@@ -24,37 +24,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   // HTML에서 요소들을 가져옵니다.
-  var stars = document.querySelectorAll(".star");
-  var ratingValue = document.getElementById("rating-value");
+  	var stars = document.querySelectorAll(".star");
+  	var ratingValue = document.getElementById("rating-value");
 
   // 초기화할 때 기본 값으로 채워줄 별 개수를 설정합니다.
-  var initialRating = Math.round(gradeAvg != null ? gradeAvg.getR_grade() : 0);
+  	var initialRating = Math.round(gradeAvg != null ? gradeAvg.getR_grade() : 0);
+	console.log("initialRating:", initialRating);
 
   // 초기화할 때 별을 채워주는 함수 호출
-  fillStars(initialRating);
+  	fillStars(initialRating);
 });
 
 // 별을 채워주는 함수
 function fillStars(rating) {
-  var stars = document.querySelectorAll(".star");
+  	var stars = document.querySelectorAll(".star");
 
   // 모든 별의 클래스 초기화
-  stars.forEach(function (star) {
+  	tars.forEach(function (star) {
     star.classList.remove("filled");
   });
 
   // 현재 클릭한 별의 이하 별들을 채웁니다.
-  for (var i = 0; i < rating; i++) {
-    stars[i].classList.add("filled");
+  	for (var i = 0; i < rating; i++) {
+    	stars[i].classList.add("filled");
   }
 }
 
 
-function maskUsername(username) {
-  if (username.length <= 3) {
-    return username;
-  } else {
-    const maskedPart = "*".repeat(username.length - 3);
-    return username.substring(0, 3) + maskedPart;
-  }
-}
+//function maskUsername(username) {
+//  if (username.length <= 3) {
+//    return username;
+//  } else {
+//    const maskedPart = "*".repeat(username.length - 3);
+//    return username.substring(0, 3) + maskedPart;
+//  }
+//}
