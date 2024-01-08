0dd1cb65301cec4f3a5f9908a027e0bd6d4dc678
leegahyun990115@gmail.com <leegahyun990115@gmail.com>
2024-01-08 17:09:51 +0900

    바로구매 버튼 완성

---
+++ src/main/java/com/recovery/account/SellerShowC.java
@@ -0,0 +1,20 @@
+package com.recovery.account;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SellerShowC")
+public class SellerShowC extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	SellerAccountDAO.SellerShow(request,response);
+	
+	}
+
+}
